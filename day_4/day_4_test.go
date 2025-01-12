4688b3fa3b1bc5896168921960d5bd3dfd8c4578
victorfleury <victor.fleury@gmail.com>
2025-01-11 20:58:31 -0500

    feat: day4 wip. Part2 to do

--- day_4/day_4_test.go
+++ day_4/day_4_test.go
@@ -11,3 +11,13 @@ func TestSoluce(t *testing.T) {
 		t.Errorf("got %v want %v with input %v", got, want, input)
 	}
 }
+
+func TestSolucePart2(t *testing.T) {
+	input := "input_test.txt"
+	got := SolucePart2(input)
+	want := 9
+
+	if got != want {
+		t.Errorf("got %v want %v with input %v", got, want, input)
+	}
+}
--- day_4/main.go
+++ day_4/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	//"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -16,16 +16,24 @@ var Search_Directions = []Direction{
 	Direction{-1, 0},
 	Direction{-1, 1},
 	Direction{0, -1},
-	Direction{0, 0},
+	//Direction{0, 0},
 	Direction{0, 1},
 	Direction{1, -1},
 	Direction{1, 0},
 	Direction{1, 1},
 }
 
+var New_Directions = []Direction{
+	Direction{-1, -1},
+	Direction{-1, 1},
+	Direction{1, -1},
+	Direction{1, 1},
+}
+
 func main() {
 	fmt.Println("Advent of code day 4")
-	Soluce("input.txt")
+	result := Soluce("input.txt")
+	fmt.Println("Result ", result)
 }
 
 func Soluce(filepath string) int {
@@ -35,39 +43,77 @@ func Soluce(filepath string) int {
 		panic(err)
 	}
 
-	grid := strings.Split(string(data), "\n")
-	fmt.Println(grid)
+	// Split and strip any empty lines
+	grid := slices.DeleteFunc(
+		strings.Split(string(data), "\n"),
+		func(e string) bool {
+			return e == ""
+		},
+	)
 
 	var result int
-	for i := 0; i < len(grid); i++ {
-		columns := len(grid[0])
-		for j := 0; j < columns; j++ {
-			if string(grid[i][j]) == "X" {
+	for row := range grid {
+		for col := range grid[0] {
+			if string(grid[row][col]) == "X" {
 				for _, d := range Search_Directions {
-					//fmt.Println(i, j, d)
-					if getWord(grid, i, j, d) == "XMAS" {
+					word := getWord(grid, row, col, d)
+					if word == "XMAS" {
 						result += 1
-						fmt.Println("Current result ", result)
 					}
 				}
 			}
 		}
 
 	}
-
 	return result
 }
 
-func getWord(grid []string, i, j int, d Direction) string {
-	word := string(grid[i][j])
+func getWord(grid []string, row, col int, d Direction) string {
+	word := string(grid[row][col])
 	for len(word) < 4 {
-		i += d.x
-		j += d.y
-		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) {
+		row += d.x
+		col += d.y
+		if row < 0 || row >= len(grid) || col < 0 || col >= len(grid[0]) {
 			return ""
 		}
-		word += string(grid[i][j])
+		word += string(grid[row][col])
 	}
-	//fmt.Println("Word", word)
 	return word
+}
+
+func SolucePart2(filepath string) int {
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	// Split and strip any empty lines
+	grid := slices.DeleteFunc(
+		strings.Split(string(data), "\n"),
+		func(e string) bool {
+			return e == ""
+		},
+	)
+	var result int
+	for row := range grid {
+		for col := range grid[0] {
+			if string(grid[row][col]) != "A" {
+				continue
+			}
+			if getX(grid, row, col) {
+				result += 1
+			}
+		}
+	}
+	return 0
+}
+
+func getX(grid []string, row, col int) bool {
+
+	for _, d := range New_Directions {
+		if !strings.Contains(string(grid[row-d.y][col-d.x]), "MS") {
+			return false
+		}
+	}
+	return true
 }
